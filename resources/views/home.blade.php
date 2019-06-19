fdb6c0b14a250d2d11abafe7a53f6a97de788f86
Roberto Arruda <roberto0arruda@hotmail.com>
2019-06-19 09:28:25 -0400

    add adminlte template - login + validaçao

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+    <small>Bem Vindo: <b>{{$user->name}}</b></small>
+@stop
+
+@section('content')
+
+    <div class="box box-success">
+
+    </div>
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Teste Freelancer TLO
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/yanzmtbr/full-stack-test/blob/master/README.md" target="_blank">O Teste</a>
+                    <a href="https://github.com/roberto0arruda/teste-tlo" target="_blank">GitHub</a>
                 </div>
             </div>
         </div>
